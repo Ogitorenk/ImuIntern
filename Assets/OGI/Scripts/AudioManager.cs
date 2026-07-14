052bedf28e659fab83e951addc137188d7f7d7d4
Ogitorenk <ogulcandanisment2001@gmail.com>
2026-07-14 04:25:00 +0300

    voice we make it we are amazing
    cembo ses yaptık la irem cambaz sound designer xd no mosic because of no money

--- Assets/OGI/Scripts/AudioManager.cs
+++ Assets/OGI/Scripts/AudioManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class AudioManager : MonoBehaviour
 {
@@ -8,12 +9,13 @@ public class AudioManager : MonoBehaviour
     public AudioClip[] footstepDirt;
     public AudioClip[] footstepStone;
     public AudioClip[] footstepWood;
-    public AudioClip jumpSound;
+
+    public AudioClip donJumpSound;
+    public AudioClip sanchoJumpSound;
     public AudioClip landSound;
     public AudioClip boxPushSound;
     public AudioClip leverSound;
 
-    // === YENÝ EKLENDÝ: EZÝCÝ TUZAK SESÝ KLÝBÝ ===
     [Tooltip("Ezici tuzak aþaðý küt diye inerken çalacak smash sesi kanka")]
     public AudioClip crusherSound;
 
@@ -28,12 +30,24 @@ public class AudioManager : MonoBehaviour
     public AudioClip arrowShootSound;
     public AudioClip drinkPotionSound;
 
+    [Header("--- ÖLÜM SESLERÝ ---")]
+    [Tooltip("Don Kiþot öldüðünde çalacak ses kanka")]
+    public AudioClip donDeathSound;
+    [Tooltip("Sancho öldüðünde çalacak ses kanka")]
+    public AudioClip sanchoDeathSound;
+
+    // === SPAM ENGELLEME SÝHRÝ: Her sesin son oynatýlma zamanýný tutan liste ===
+    private Dictionary<AudioClip, float> soundCooldowns = new Dictionary<AudioClip, float>();
+
+    [Header("Spam Hassasiyet Ayarý")]
+    [Tooltip("Ayný ses klibinin tekrar çalabilmesi için aradan geçmesi gereken minimum saniye kanka.")]
+    public float globalSpamCooldown = 0.15f;
+
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-            // Level designer sahneleri arasýnda geçiþ yaparken ses sisteminin zýnk diye kopmasýný engelliyoruz kanka
             DontDestroyOnLoad(gameObject);
         }
         else
@@ -42,14 +56,58 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    // === AKILLI HASAR VE ÖLÜM SES SÝSTEMÝ ===
+    // Karakter hasar aldýðýnda kendi can scriptinden bu fonksiyonu çaðýr kanka.
+    // Örnek kullaným: AudioManager.Instance.PlayCharacterDamageOrDeath("Don", currentHealth, position);
+    public void PlayCharacterDamageOrDeath(string characterName, float currentHealth, Vector3 position = default, float volume = 1f)
+    {
+        if (characterName == "Don")
+        {
+            if (currentHealth <= 0)
+            {
+                // Caný sýfýra veya altýna düþtüyse sadece ölüm sesini çal
+                PlaySound(donDeathSound, position, volume);
+            }
+            else
+            {
+                // Hala yaþýyorsa normal hasar sesini çal
+                PlaySound(donDamageSound, position, volume);
+            }
+        }
+        else if (characterName == "Sancho")
+        {
+            if (currentHealth <= 0)
+            {
+                PlaySound(sanchoDeathSound, position, volume);
+            }
+            else
+            {
+                PlaySound(sanchoDamageSound, position, volume);
+            }
+        }
+    }
+
     // 2D veya 3D ses çalma fonksiyonu (Konum verilirse 3D, verilmezse 2D çalar kanka)
     public void PlaySound(AudioClip clip, Vector3 position = default, float volume = 1f)
     {
         if (clip == null) return;
 
+        // === SPAM KORUMASI KONTROLÜ KANKA ===
+        if (soundCooldowns.TryGetValue(clip, out float lastPlayedTime))
+        {
+            // Eðer sesten hemen sonra geçen süre belirlediðimiz cooldown'dan küçükse sesi çalma, engelle kanka!
+            if (Time.time - lastPlayedTime < globalSpamCooldown)
+            {
+                return;
+            }
+        }
+
+        // Zamaný güncelle veya yoksa listeye ekle
+        soundCooldowns[clip] = Time.time;
+
+        // Oynatma mantýðý canavar gibi devam ediyor kanka:
         if (position == default)
         {
-            // Arayüz veya genel sesler için 2D çal kanka
             if (Camera.main != null)
             {
                 AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position, volume);
@@ -57,7 +115,6 @@ public class AudioManager : MonoBehaviour
         }
         else
         {
-            // Karakter sesleri ve akýllý 3D menzilli tuzaklar için tam koordinatýnda belirlenen volume ile çal kanka
             AudioSource.PlayClipAtPoint(clip, position, volume);
         }
     }
@@ -76,8 +133,14 @@ public class AudioManager : MonoBehaviour
 
         if (targetArray != null && targetArray.Length > 0)
         {
-            AudioClip randomClip = targetArray[Random.Range(0, targetArray.Length)];
-            PlaySound(randomClip, position, 0.4f); // Yürüme sesi kafayý ütülemesin diye volume çýtýr kýsýk kanka
+            ClipAndPlayRandom(targetArray, position);
         }
+    }
+
+    private void ClipAndPlayRandom(AudioClip[] targetArray, Vector3 position)
+    {
+        AudioClip randomClip = targetArray[Random.Range(0, targetArray.Length)];
+        // Yürüme sesleri PlaySound üzerinden geçeceði için burasý da otomatik spam filtreli kanka!
+        PlaySound(randomClip, position, 0.4f);
     }
 }
